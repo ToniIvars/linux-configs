bf6ea414d8489382178985c2f93d13d91ca477fd
ToniIvars <toniivaju@gmail.com>
2021-12-07 13:37:44 +0100

    Remove notify widget and add dunst configuration

--- qtile/config.py
+++ qtile/config.py
@@ -252,8 +252,6 @@ screens = [
                 left_sep(colours["grey"], colours["clock"]),
                 widget.Battery(charge_char='', full_char='', update_interval=30, discharge_char='', format='{char} {percent:2.0%}',
                                low_foreground=colours["red"], background=colours["grey"]),
-
-                widget.Notify(background=colours["grey"]),
             ],
             24,
             background='ffffff.0' # Only to set background opacity to 0%
