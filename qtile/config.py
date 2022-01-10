45ec3c189e897259db299f96730d26e2efa4ef64
ToniIvars <toniivaju@gmail.com>
2022-01-10 23:17:03 +0100

    Add new kitty config and update qtile config, as running in Arch Linux

--- qtile/config.py
+++ qtile/config.py
@@ -1,30 +1,4 @@
-# Copyright (c) 2010 Aldo Cortesi
-# Copyright (c) 2010, 2014 dequis
-# Copyright (c) 2012 Randall Ma
-# Copyright (c) 2012-2014 Tycho Andersen
-# Copyright (c) 2012 Craig Barnes
-# Copyright (c) 2013 horsik
-# Copyright (c) 2013 Tao Sauvage
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
-from typing import List  # noqa: F401
+from typing import List
 
 from libqtile import bar, layout, widget, qtile
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
@@ -34,12 +8,14 @@ from libqtile.widget import base
 import subprocess, os, json
 
 mod = "mod4"
-terminal = 'alacritty'
+terminal = 'kitty'
 wallpaper = 'nebula'
+
+dmenu_command = 'dmenu_run -p "Launch:" -nf "#ffffff" -sf "#ffffff" -nb "#49557D" -sb "#7b8cac" -h 26'
 
 keys = [
     # Shortcuts
-    Key([mod], "d", lazy.spawn("dmenu_run -p 'Launch: '")),
+    Key([mod], "d", lazy.spawn(dmenu_command)),
     Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
     Key([mod], "b", lazy.spawn('firefox')),
     Key([mod], "v", lazy.spawn('code')),
@@ -62,8 +38,7 @@ keys = [
     Key([mod], "space", lazy.layout.next(),
         desc="Move window focus to other window"),
 
-    # Move windows between left/right columns or move up/down in current stack.
-    # Moving out of range in Columns layout will create new column.
+    # Move windows
     Key([mod, "shift"], "Left", lazy.layout.shuffle_left(),
         desc="Move window to the left"),
     Key([mod, "shift"], "Right", lazy.layout.shuffle_right(),
@@ -72,8 +47,7 @@ keys = [
         desc="Move window down"),
     Key([mod, "shift"], "Up", lazy.layout.shuffle_up(), desc="Move window up"),
 
-    # Grow windows. If current window is on the edge of screen and direction
-    # will be to screen edge - window would shrink.
+    # Grow windows
     Key([mod, "control"], "Left", lazy.layout.grow_left(),
         desc="Grow window to the left"),
     Key([mod, "control"], "Right", lazy.layout.grow_right(),
@@ -82,64 +56,52 @@ keys = [
         desc="Grow window down"),
     Key([mod, "control"], "Up", lazy.layout.grow_up(), desc="Grow window up"),
 
-    Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
-
-    # Toggle between split and unsplit sides of stack.
-    # Split = all windows displayed
-    # Unsplit = 1 window displayed, like Max layout, but still with
-    # multiple stack panes
-    Key([mod, "shift"], "Return", lazy.layout.toggle_split(),
-        desc="Toggle between split and unsplit sides of stack"),
+    # Grow windows in the monadtall layout
+    Key([mod, "control"], "Up", lazy.layout.grow(),
+        desc="Grow window to the left"),
+    Key([mod, "control"], "Down", lazy.layout.shrink(),
+        desc="Grow window to the right"),
 
     # Toggle between different layouts as defined below
     Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
     Key([mod], "F4", lazy.window.kill(), desc="Kill focused window"),
-    Key(["mod1"], "F4", lazy.window.kill(), desc="Kill focused window"),
-
-    Key([mod, "shift"], "r", lazy.restart(), desc="Restart Qtile"),
-    Key([mod, "shift"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
-    Key([mod], "r", lazy.spawncmd(),
-        desc="Spawn a command using a prompt widget"),
 
     # Shutdown and restart
+    Key([mod, "shift"], "r", lazy.restart(), desc="Restart Qtile"),
+    Key([mod, "shift"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
+
     Key([mod, "control"], "q", lazy.spawn('shutdown now')),
     Key([mod, "control"], "r", lazy.spawn('shutdown -r now')),
     Key([mod, "control"], "s", lazy.spawn('systemctl suspend'))
 ]
 
-# The icons are icons from Nerd Fonts
-groups = [Group(i, layout='monadtall') for i in ""]
+# The icons from Nerd Fonts
+groups = [Group('', layout='columns')] + [Group(i, layout='monadtall') for i in ""]
 
 for int_key, i in enumerate(groups, start=1):
     keys.extend([
-        # mod1 + letter of group = switch to group
+        # mod + letter of group => switch to group
         Key([mod], str(int_key), lazy.group[i.name].toscreen(),
             desc="Switch to group {}".format(i.name)),
 
-        # mod1 + shift + letter of group = switch to & move focused window to group
+        # mod1 + shift + letter of group => switch to & move focused window to group
         Key([mod, "shift"], str(int_key), lazy.window.togroup(i.name, switch_group=True),
             desc="Switch to & move focused window to group {}".format(i.name)),
-        # Or, use below if you prefer not to switch to that group.
-        # # mod1 + shift + letter of group = move focused window to group
-        # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
-        #     desc="move focused window to group {}".format(i.name)),
     ])
 
+# Set the colour theme depending on the wallpaper name
+with open(os.path.expanduser('~/.config/qtile/colours.json'), 'r') as f:
+    colours = json.load(f)[wallpaper]
+
 layouts = [
-    layout.MonadTall(margin=2, single_border_width=0),
-    layout.Bsp(margin=2),
-    # layout.Columns(margin=2),
-    layout.Max(),
-    # layout.Stack(num_stacks=2),
-    # layout.Matrix(margin=2),
-    # layout.MonadWide(),
-    # layout.RatioTile(),
-    # layout.Tile(),
-    # layout.TreeTab(),
-    layout.VerticalTile(margin=2),
-    # layout.Zoomy(),
+    layout.MonadTall(margin=2, single_border_width=0,
+                     border_focus=colours["bluetooth"], border_normal=colours["clock"]),
+    layout.Columns(margin=2, border_focus=colours["bluetooth"], border_focus_stack=colours["bluetooth"],
+                   border_normal=colours["clock"], border_normal_stack=colours["clock"], grow_amount=5),
+    layout.Max()
 ]
 
+# Custom widgets
 class SSID(base.InLoopPollText):
     def __init__(self, name_display=12, **config):
         base.InLoopPollText.__init__(self, "", **config)
@@ -190,10 +152,7 @@ class Brightness(base.InLoopPollText):
         current = float(subprocess.check_output(['light', '-G']).decode())
         return f' {round(current, -1):.0f}%'
 
-# Set the colour theme depending on the wallpaper name
-with open(os.path.expanduser('~/.config/qtile/colours.json'), 'r') as f:
-    colours = json.load(f)[wallpaper]
-
+# Widgets defaults
 widget_defaults = dict(
     font='SauceCodePro Nerd Font Mono Semibold',
     fontsize=18,
@@ -202,9 +161,12 @@ widget_defaults = dict(
 )
 extension_defaults = widget_defaults.copy()
 
+# Definition of screen and widgets
 ssid_widget = SSID(mouse_callbacks={'Button1': lambda: qtile.cmd_spawn(f'{terminal} -e nmtui'.split())}, background=colours["wifi"])
+
 bluetooth_widget = Bluetooth(mouse_callbacks={'Button1': lambda: qtile.cmd_spawn(f'{terminal} -e bluetooth_config'.split())},
                    max_chars=14, background=colours["bluetooth"])
+
 brightness_widget = Brightness(background=colours["volume"])
 
 left_sep = lambda fg, bg: widget.TextBox(text='', foreground=fg, background=bg, fontsize=56, padding=-11) 
@@ -224,8 +186,8 @@ screens = [
                 widget.CheckUpdates(
                     display_format = ' {updates}',
                     no_update_string = ' 0',
-                    custom_command = 'apt list --upgradable', custom_command_modify = lambda x: x-1,
-                    mouse_callbacks = {'Button1': lambda: qtile.cmd_spawn(f'{terminal} -e sudo apt upgrade'.split())},
+                    distro= 'Arch',
+                    mouse_callbacks = {'Button1': lambda: qtile.cmd_spawn(f'{terminal} -e sudo pacman -Su --noconfirm'.split())},
                     restart_indicator = 'ﰇ',
                     colour_have_updates = colours["red"], colour_no_updates = colours["black"], background=colours["white"]
                 ),
@@ -259,46 +221,29 @@ screens = [
     ),
 ]
 
-# Drag floating layouts.
-mouse = [
-    Drag([mod], "Button1", lazy.window.set_position_floating(),
-         start=lazy.window.get_position()),
-    Drag([mod], "Button3", lazy.window.set_size_floating(),
-         start=lazy.window.get_size()),
-    Click([mod], "Button2", lazy.window.bring_to_front())
-]
-
 dgroups_key_binder = None
 dgroups_app_rules = []  # type: List
 follow_mouse_focus = True
 bring_front_click = False
 cursor_warp = False
-floating_layout = layout.Floating(float_rules=[
-    # Run the utility of `xprop` to see the wm class and name of an X client.
-    *layout.Floating.default_float_rules,
-    Match(wm_class='confirmreset'),  # gitk
-    Match(wm_class='makebranch'),  # gitk
-    Match(wm_class='maketag'),  # gitk
-    Match(wm_class='ssh-askpass'),  # ssh-askpass
-    Match(title='branchdialog'),  # gitk
-    Match(title='pinentry'),  # GPG key password entry
-])
+
+# floating_layout = layout.Floating(float_rules=[
+#     # Run the utility of `xprop` to see the wm class and name of an X client.
+#     *layout.Floating.default_float_rules,
+#     Match(wm_class='confirmreset'),  # gitk
+#     Match(wm_class='makebranch'),  # gitk
+#     Match(wm_class='maketag'),  # gitk
+#     Match(wm_class='ssh-askpass'),  # ssh-askpass
+#     Match(title='branchdialog'),  # gitk
+#     Match(title='pinentry'),  # GPG key password entry
+# ])
+
 auto_fullscreen = True
 focus_on_window_activation = "smart"
 reconfigure_screens = True
 
-# If things like steam games want to auto-minimize themselves when losing
-# focus, should we respect this or not?
 auto_minimize = True
 
-# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
-# string besides java UI toolkits; you can see several discussions on the
-# mailing lists, GitHub issues, and other WM documentation that suggest setting
-# this string if your java app doesn't work correctly. We may as well just lie
-# and say that we're a working one by default.
-#
-# We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
-# java that happens to be on java's whitelist.
 # wmname = "LG3D"
 
 commands = [
