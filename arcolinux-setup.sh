95ccb31098ce68d59111433e274e80122928bf55
ToniIvars <toniivaju@gmail.com>
2022-05-14 19:02:57 +0200

    Add redirection to the /dev/null only when copying files or when dealing with zsh stuff

--- arcolinux-setup.sh
+++ arcolinux-setup.sh
@@ -9,39 +9,39 @@ sudo snap install code --classic
 
 # i3 config
 echo -e "[+] Copying i3 and kitty config...\n"
-cp -r i3 ~/.config/
+cp -r i3 ~/.config/ &>/dev/null
 
 # Kitty config
-cp -r kitty ~/.config/
+cp -r kitty ~/.config/ &>/dev/null
 
 # ZSH config
 echo -e "[+] Installing oh-my-zsh and its plugins...\n"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended &>/dev/null
 
-git clone --depth=1 https://github.com/romkatv/powerlevel10k ~/.oh-my-zsh/custom/themes/powerlevel10k
-cp zsh/.p10k.zsh ~/.p10k.zsh
+git clone --depth=1 https://github.com/romkatv/powerlevel10k ~/.oh-my-zsh/custom/themes/powerlevel10k &>/dev/null
+cp zsh/.p10k.zsh ~/.p10k.zsh &>/dev/null
 
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions &>/dev/null
 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting &>/dev/null
 
 echo -e "[+] Copying polybar, rofi, picom, dunst, feh and zsh config...\n"
 # Polybar config
-cp -r polybar ~/.config/
+cp -r polybar ~/.config/ &>/dev/null
 
 # Rofi config
-cp -r rofi ~/.config/
+cp -r rofi ~/.config/ &>/dev/null
 
 # Picom config
-cp -r picom ~/.config/
+cp -r picom ~/.config/ &>/dev/null
 
 # Dunst config
-cp -r dunst ~/.config/
+cp -r dunst ~/.config/ &>/dev/null
 
 # Feh config
-cp -r feh ~/.config/
+cp -r feh ~/.config/ &>/dev/null
 
-cp zsh/.zshrc ~/.zshrc
+cp zsh/.zshrc ~/.zshrc &>/dev/null
 
 echo -e "[+] Changing default shell to zsh...\n"
 chsh -s /usr/bin/zsh
