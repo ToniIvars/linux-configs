c4079af7d09e1ebbce7cb4fdd1992f0f0118d3ba
ToniIvars <toniivaju@gmail.com>
2022-05-14 18:34:23 +0200

    Remove redirection to the /dev/null in each command except the logging

--- arcolinux-setup.sh
+++ arcolinux-setup.sh
@@ -2,46 +2,46 @@
 
 #Installation
 echo -e "[+] Installing some software with paru...\n"
-paru -S wget firefox kitty dunst zsh picom light alsa-utils blueberry feh lsd bat rofi libnotify thunar gvfs thunar-archive-plugin thunar-media-tags-plugin thunar-volman tumbler libgsf gvfs-mtp ttf-fira-code nerd-fonts-source-code-pro polybar snapd --noconfirm &>/dev/null
+paru -S wget firefox kitty dunst zsh picom light alsa-utils blueberry feh lsd bat rofi libnotify thunar gvfs thunar-archive-plugin thunar-media-tags-plugin thunar-volman tumbler libgsf gvfs-mtp ttf-fira-code nerd-fonts-source-code-pro polybar snapd --noconfirm
 
 echo -e "[+] Installing VS Code with snap...\n"
-sudo snap install code --classic &>/dev/null
+sudo snap install code --classic
 
 # i3 config
 echo -e "[+] Copying i3 and kitty config...\n"
-cp -r i3 ~/.config/ &>/dev/null
+cp -r i3 ~/.config/
 
 # Kitty config
-cp -r kitty ~/.config/ &>/dev/null
+cp -r kitty ~/.config/
 
 # ZSH config
 echo -e "[+] Installing oh-my-zsh and its plugins...\n"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended &>/dev/null
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
-git clone --depth=1 https://github.com/romkatv/powerlevel10k ~/.oh-my-zsh/custom/themes/powerlevel10k &>/dev/null
-cp zsh/.p10k.zsh ~/.p10k.zsh &>/dev/null
+git clone --depth=1 https://github.com/romkatv/powerlevel10k ~/.oh-my-zsh/custom/themes/powerlevel10k
+cp zsh/.p10k.zsh ~/.p10k.zsh
 
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions &>/dev/null
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting &>/dev/null
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
 echo -e "[+] Copying polybar, rofi, picom, dunst, feh and zsh config...\n"
 # Polybar config
-cp -r polybar ~/.config/ &>/dev/null
+cp -r polybar ~/.config/
 
 # Rofi config
-cp -r rofi ~/.config/ &>/dev/null
+cp -r rofi ~/.config/
 
 # Picom config
-cp -r picom ~/.config/ &>/dev/null
+cp -r picom ~/.config/
 
 # Dunst config
-cp -r dunst ~/.config/ &>/dev/null
+cp -r dunst ~/.config/
 
 # Feh config
-cp -r feh ~/.config/ &>/dev/null
+cp -r feh ~/.config/
 
-cp zsh/.zshrc ~/.zshrc &>/dev/null
+cp zsh/.zshrc ~/.zshrc
 
 echo -e "[+] Changing default shell to zsh...\n"
-chsh -s /usr/bin/zsh &>/dev/null
+chsh -s /usr/bin/zsh
