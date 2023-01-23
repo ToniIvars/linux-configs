6eb25d2cacdb0e19db0a81f42a34a1d694c3e8e9
ToniIvars <toniivaju@gmail.com>
2023-01-23 15:52:28 +0100

    Update setup script and README

--- README.md
+++ README.md
@@ -1,7 +1,22 @@
 # linux-configs
+
 This repository is created to publish configuration files of some software in linux.
 
+## Automatic configuration
+
+If you are using Arch Linux, a bash script is provided to automate the installation of the configuration I am using at the moment.
+
+In order to run it, execute the following commands:
+
+```bash
+git clone https://github.com/ToniIvars/linux-configs
+cd linux-configs
+chmod +x arch-linux-setup.sh
+./arch-linux-setup.sh
+```
+
 ## Default software
+
 - Web browser: Firefox
 - Code editor: Visual Studio Code
 - File manager: Thunar
--- arch-linux-setup.sh
+++ arch-linux-setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Activate parallel downloads
+echo -e "Activating pacman parallel downloads...\n"
+sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 5/g' /etc/pacman.conf
+
 # Paru installation
 echo -e "[+] Installing paru...\n"
 git clone https://aur.archlinux.org/paru.git
@@ -10,11 +14,7 @@ rm -rf paru
 
 #Installation
 echo -e "[+] Installing some software with paru...\n"
-paru -S python python-pip python-setuptools wget firefox kitty dunst zsh picom light alsa-utils blueberry feh lsd bat rofi libnotify thunar gvfs thunar-archive-plugin thunar-media-tags-plugin thunar-volman tumbler libgsf gvfs-mtp ttf-fira-code nerd-fonts-source-code-pro polybar snapd unzip --noconfirm
-
-sudo systemctl enable --now snapd.socket
-sudo systemctl start snapd.socket
-sudo ln -s /var/lib/snapd/snap /snap
+paru -S python python-pip python-setuptools wget firefox kitty dunst zsh picom light alsa-utils blueberry feh lsd bat rofi libnotify thunar gvfs thunar-archive-plugin thunar-media-tags-plugin thunar-volman tumbler libgsf gvfs-mtp ttf-fira-code ttf-sourcecodepro-nerd polybar unzip lightdm-slick-greeter --noconfirm
 
 # i3 config
 echo -e "[+] Copying i3 and kitty config...\n"
@@ -34,7 +34,12 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/p
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting &>/dev/null
 
-echo -e "[+] Copying polybar, rofi, picom, dunst, feh and zsh config...\n"
+echo -e "[+] Copying lightdm, polybar, rofi, picom, dunst, feh and zsh config...\n"
+# LightDM config
+cp lightdm/lightdm.conf /etc/lightdm/
+cp lightdm/slick-greeter.conf /etc/lightdm/
+cp feh/nebula.jpg /usr/share/backgrounds/
+
 # Polybar config
 cp -r polybar ~/.config/ &>/dev/null
 
@@ -55,13 +60,32 @@ cp zsh/.zshrc ~/.zshrc &>/dev/null
 echo -e "[+] Changing default shell to zsh...\n"
 chsh -s /usr/bin/zsh
 
-echo -e "[+] Installing VS Code with snap...\n"
-sudo snap install code --classic
-
 echo -e "[+] Installing lxappearance and some themes...\n"
-paru -S bibata-cursor-theme-bin breeze-icons lxappearance-gtk3 breeze-gtk arc-gtk-theme arc-icon-theme arc-darkest-theme-git papirus-icon-theme --noconfirm
+paru -S breeze-icons lxappearance-gtk3 breeze-gtk arc-darkest-theme-git papirus-icon-theme --noconfirm
 
 echo -e "[+] Installing Vimix GRUB theme...\n"
 unzip vimix-theme.zip
 sudo mv grub2-theme-vimix-master/Vimix /boot/grub/themes 2>/dev/null
 sudo grub-mkconfig -o /boot/grub/grub.cfg
+
+echo -e "[+] Installing Snapd and VSCode...\n"
+paru -S snapd --noconfirm
+
+sudo systemctl enable --now snapd.socket
+sudo systemctl start snapd.socket
+sudo ln -s /var/lib/snapd/snap /snap
+sudo systemctl enable --now snapd.apparmor
+
+sudo snap install code --classic
+
+echo -e "Copying the timer to automate the system update...\n"
+
+cp timers/sysupdate.* /etc/systemd/system/
+cp timers/system-update.sh /usr/local/bin/
+chmod +x /usr/local/bin/system-update.sh
+
+sudo systemctl enable --now sysupdate.timer
+sudo systemctl start sysupdate.timer
+
+echo "Now it is recommended that you reboot your system"
+exit 0
