e8f765e3c791bc08f282bc1d87dabf34f34e5bdd
ToniIvars <toniivaju@gmail.com>
2023-01-23 19:02:42 +0100

    Update setup script

--- arch-linux-setup.sh
+++ arch-linux-setup.sh
@@ -2,13 +2,13 @@
 
 # Activate parallel downloads
 echo -e "Activating pacman parallel downloads...\n"
-sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 5/g' /etc/pacman.conf
+sudo sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 5/g' /etc/pacman.conf
 
 # Paru installation
 echo -e "[+] Installing paru...\n"
 git clone https://aur.archlinux.org/paru.git
 cd paru
-makepkg -si
+makepkg -si --noconfirm
 cd ..
 rm -rf paru
 
@@ -18,44 +18,46 @@ paru -S python python-pip python-setuptools wget firefox kitty dunst zsh picom l
 
 # i3 config
 echo -e "[+] Copying i3 and kitty config...\n"
-cp -r i3 ~/.config/ &>/dev/null
+cp -r i3 ~/.config/
 
 # Kitty config
-cp -r kitty ~/.config/ &>/dev/null
+cp -r kitty ~/.config/
 
 # ZSH config
 echo -e "[+] Installing oh-my-zsh and its plugins...\n"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended &>/dev/null
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended 
 
-git clone --depth=1 https://github.com/romkatv/powerlevel10k ~/.oh-my-zsh/custom/themes/powerlevel10k &>/dev/null
-cp zsh/.p10k.zsh ~/.p10k.zsh &>/dev/null
+git clone --depth=1 https://github.com/romkatv/powerlevel10k ~/.oh-my-zsh/custom/themes/powerlevel10k 
+cp zsh/.p10k.zsh ~/.p10k.zsh 
 
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions &>/dev/null
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions 
 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting &>/dev/null
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting 
+
+cp zsh/.zshrc ~/.zshrc
 
 echo -e "[+] Copying lightdm, polybar, rofi, picom, dunst, feh and zsh config...\n"
 # LightDM config
-cp lightdm/lightdm.conf /etc/lightdm/
-cp lightdm/slick-greeter.conf /etc/lightdm/
+sudo cp lightdm/lightdm.conf /etc/lightdm/
+sudo cp lightdm/slick-greeter.conf /etc/lightdm/
+
+mkdir -p /usr/share/backgrounds
 cp feh/nebula.jpg /usr/share/backgrounds/
 
 # Polybar config
-cp -r polybar ~/.config/ &>/dev/null
+cp -r polybar ~/.config/ 
 
 # Rofi config
-cp -r rofi ~/.config/ &>/dev/null
+cp -r rofi ~/.config/ 
 
 # Picom config
-cp -r picom ~/.config/ &>/dev/null
+cp -r picom ~/.config/ 
 
 # Dunst config
-cp -r dunst ~/.config/ &>/dev/null
+cp -r dunst ~/.config/ 
 
 # Feh config
-cp -r feh ~/.config/ &>/dev/null
-
-cp zsh/.zshrc ~/.zshrc &>/dev/null
+cp -r feh ~/.config/ 
 
 echo -e "[+] Changing default shell to zsh...\n"
 chsh -s /usr/bin/zsh
@@ -68,6 +70,15 @@ unzip vimix-theme.zip
 sudo mv grub2-theme-vimix-master/Vimix /boot/grub/themes 2>/dev/null
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 
+echo -e "Copying the timer to automate the system update...\n"
+
+cp timers/sysupdate.* /etc/systemd/system/
+cp timers/system-update.sh /usr/local/bin/
+chmod +x /usr/local/bin/system-update.sh
+
+sudo systemctl enable --now sysupdate.timer
+sudo systemctl start sysupdate.timer
+
 echo -e "[+] Installing Snapd and VSCode...\n"
 paru -S snapd --noconfirm
 
@@ -77,15 +88,6 @@ sudo ln -s /var/lib/snapd/snap /snap
 sudo systemctl enable --now snapd.apparmor
 
 sudo snap install code --classic
-
-echo -e "Copying the timer to automate the system update...\n"
-
-cp timers/sysupdate.* /etc/systemd/system/
-cp timers/system-update.sh /usr/local/bin/
-chmod +x /usr/local/bin/system-update.sh
-
-sudo systemctl enable --now sysupdate.timer
-sudo systemctl start sysupdate.timer
 
 echo "Now it is recommended that you reboot your system"
 exit 0
