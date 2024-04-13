315ed2b7f75b7a32e3c42b33d805afa45dabb9f2
Toni Ivars <toniivaju@gmail.com>
2024-04-13 10:58:31 +0200

    Modify msi_backup to open a new Firefox tab with Google Drive

--- msi_backup.sh
+++ msi_backup.sh
@@ -3,7 +3,8 @@
 FNAME="Archivos_$(date +'%d-%m-%Y').zip"
 mkdir /tmp/msi_backup
 
-notify-send -a 'MSI Backup' 'Info' 'Compressing "Archivos" in a ZIP file...'
+# notify-send -a 'MSI Backup' 'Info' 'Compressing "Archivos" in a ZIP file...'
 zip -r "/tmp/msi_backup/$FNAME" /home/toni/Archivos -x '*env/*' -x '*venv/*' -x '*node_modules*' -x '*dist/*' -x '*buildozer/*' -x '*__pycache__*' -x '*.cia' -x '*.3ds'
 
 thunar /tmp/msi_backup & disown
+firefox 'https://drive.google.com/'
