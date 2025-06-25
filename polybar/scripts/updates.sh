091f4a154f72437c6a2d94cb0ed0bff3e2275c96
Toni Ivars <toniivaju@gmail.com>
2025-06-25 11:09:31 +0200

    Update updates polybar scripts

--- polybar/scripts/updates.sh
+++ polybar/scripts/updates.sh
@@ -1,9 +1,8 @@
 #/bin/bash
 
-updates_error=$(timeout 1 pacman -Q -u 2>&1 | grep "error" | wc -l)
 updates=$(timeout 2 pacman -Q -u | grep -vE "\[ignorado\]|\[ignored\]" | wc -l)
 
-if [ $updates_error -gt 0 ]; then
+if [ $? -gt 0 ]; then
 	echo "%{F#ee0000}󰁈 Error"
 elif [ $updates -gt 0 ]; then
 	echo "%{F#ee0000}󰁈 $updates"
